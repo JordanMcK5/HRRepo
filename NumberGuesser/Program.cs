c68422fca1e038dc6ed0b0a1c077e746df689642
JordanMcK5 <88345208+JordanMcK5@users.noreply.github.com>
2022-04-05 01:26:57 +0100

    Initial Number Guesser Commit

---
+++ NumberGuesser/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace NumberGuesser
+{
+
+// Main Class
+    class Program
+    {
+//Entry Point Method
+        public static void Main(string[] args)
+        {
+            // Start main code here //
+            Console.WriteLine("Hello World");
+}
+    }
+}
