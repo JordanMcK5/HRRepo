7b9e2fac06c1458ee6f0e083e722b15607caad17
JordanMcK5 <88345208+JordanMcK5@users.noreply.github.com>
2022-04-05 02:00:14 +0100

    Pokemon Game created

--- NumberGuesser/Program.cs
+++ NumberGuesser/Program.cs
@@ -6,11 +6,55 @@ namespace NumberGuesser
 // Main Class
     class Program
     {
-//Entry Point Method
+        //Entry Point Method
         public static void Main(string[] args)
         {
-            // Start main code here //
-            Console.WriteLine("Hello World");
-}
+
+            //Set app variables
+            string appName = "Number Guesser!";
+            string appAuthor = "Jordan McKenna";
+
+            //Change Text Colour
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"{appName} by {appAuthor}");
+
+            Console.ResetColor();
+
+            //User name
+
+            Console.WriteLine("What is your name?");
+            string inputName = Console.ReadLine();
+            Console.WriteLine($"Hey {inputName}!, Have a go a guessing my favourite Pokemon!");
+
+            // fave Pokemon
+            string favPokemon = "Charizard";
+
+            string guess = "";
+
+            while(guess != favPokemon)
+            {
+                string input = Console.ReadLine();
+
+                guess = (input);
+
+                // match guess to fave Pokemon
+
+                if(guess != favPokemon)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+
+                    Console.WriteLine($"That aint my favourite Pokemon, {inputName}. Try again!");
+
+                    Console.ResetColor();
+
+                }
+            }
+
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            Console.WriteLine($"Rawrrrrr! Well done {inputName}, {favPokemon} is my favourite Pokemon");
+
+            Console.ResetColor();
+        }
     }
 }
